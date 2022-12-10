d5801cde7dccdff67f845b8f25291f3038f15790
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2022-12-10 00:01:55 -0500

    add card success preview

--- app/src/main/java/com/jdlstudios/alertdialogsapp/MainActivity.kt
+++ app/src/main/java/com/jdlstudios/alertdialogsapp/MainActivity.kt
@@ -1,39 +1,33 @@
 package com.jdlstudios.alertdialogsapp
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.app.Dialog
-import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.jdlstudios.alertdialogsapp.databinding.ActivityMainBinding
+import com.jdlstudios.alertdialogsapp.databinding.MyCustomDialogBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var binding2: MyCustomDialogBinding
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnAlert.setOnClickListener {
-            val alertDialog  = layoutInflater.inflate(R.layout.my_custom_dialog, null)
+            binding2 = MyCustomDialogBinding.inflate(layoutInflater)
             val myDialog = Dialog(this)
-            myDialog.setContentView(alertDialog)
+            myDialog.setContentView(binding2.root)
             myDialog.setCancelable(true)
             myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
             myDialog.show()
 
-            val btn = alertDialog?.findViewById<Button>(R.id.btnTest)
-            btn?.setOnClickListener {
+            binding2.btnTest.setOnClickListener {
                 binding.txtTest.text= "asdf"
-                Toast.makeText(this@MainActivity,"hola!",Toast.LENGTH_SHORT).show()
                 myDialog.dismiss()
             }
         }
